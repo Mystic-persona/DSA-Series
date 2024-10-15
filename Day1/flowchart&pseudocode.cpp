6aabea4017fe7d60274751280babfe04f7e39ff9
Mystic-persona <sushreesangeeta2005@gmail.com>
2024-10-15 19:19:47 +0530

    Added files

---
+++ Day1/flowchart&pseudocode.cpp
@@ -0,0 +1,25 @@
+//How to solve problems?
+
+// 1-> Understand the problem
+// 2-> Input
+// 3-> Solution
+// 4-> Code
+
+//Flowchart (Diagram of a solution)
+
+//shapes used in flowchart
+
+//   start and exit/end -> oval
+//   input/output  and print -> parallelogram 
+//   process -> rectangle
+//   decision(condition) -> diamond 
+//   for connecting the steps we use arrow(---->)
+
+//Pseudocode (General logic of solution)
+
+// pseudocode for sum of 2 numbers
+// 1->input a & b
+// 2->sum = a + b
+// 3->print sum
+// 4->Exit
+
---
+++ Day1/practiceqs.cpp
@@ -0,0 +1,45 @@
+//Pseudocode
+
+// Area of a Square
+//1. input a
+//2. area = a*a
+//3. print area
+//4. Exit
+
+// min of 2 numbers
+//1. input a & b 
+//2. if a < b
+        // print a
+    //else
+        // print b   
+//3. Exit
+
+// Is number Odd or Even
+//1. input n
+//2. If n%2 == 0
+          //print even
+    //else     
+          //print odd
+//3. Exit
+
+// Sum of numbers from 1 to N
+//1. Input n
+//2. count = 1, Sum = 0
+//3. while count <= N
+        //sum = sum + count
+        //count = count + 1
+//4. print sum
+//5. Exit
+
+//Is number prime or not 
+//1. input n
+//2. i=2
+//3. while i<=(n-1)
+         //if n%i == 0
+                 //print non-prime
+                 //exit
+        //else
+            //i += 1
+//4. print prime
+//5. exit
+            
