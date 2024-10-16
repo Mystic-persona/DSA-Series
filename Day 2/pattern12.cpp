770ee9731485dd638e360aa793ea0e4772140b3b
Mystic-persona <sushreesangeeta2005@gmail.com>
2024-10-16 12:26:56 +0530

    commit changes

---
+++ Day 2/pattern12.cpp
@@ -0,0 +1,17 @@
+//Floyd's Triangle Pattern with numbers
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n =4;
+    int num=1;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < i+1; j++) {  //backward--(j=i+1; j>0; j--)
+        cout<<num<<" ";
+        num++;
+      }
+      cout<<endl;
+    }
+    return 0;
+}
