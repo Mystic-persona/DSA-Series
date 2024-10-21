b6667209b3336297de8380681e2915d607976dd4
Mystic-persona <sushreesangeeta2005@gmail.com>
2024-10-22 04:29:15 +0530

    Added file

--- Day 2/pattern12.cpp
+++ Day 2/pattern12.cpp
@@ -1,17 +1,16 @@
-//Floyd's Triangle Pattern with numbers
-
+//Reverse Triangle pattern with characters
+//vertically increasing and horizontally decreasing 
 #include<iostream>
 using namespace std;
 
-int main(){
-    int n =4;
-    int num=1;
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < i+1; j++) {  //backward--(j=i+1; j>0; j--)
-        cout<<num<<" ";
-        num++;
-      }
-      cout<<endl;
+int main (){
+    int n = 4;
+   
+    for(int i =1; i<=n;i++){
+        for(int j=i; j>=1; j--){
+        cout<< char('A' + j -1)<<" ";
+        }
+        cout<<endl;
     }
     return 0;
 }
