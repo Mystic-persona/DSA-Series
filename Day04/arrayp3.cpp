8a9f3c7aeddc86cce9a73830a10f1a6a16f21fda
Mystic-persona <sushreesangeeta2005@gmail.com>
2024-11-08 00:06:25 +0530

    Added file

--- Day04/arrayp3.cpp
+++ Day04/arrayp3.cpp
@@ -4,17 +4,20 @@
 using namespace std;
 void getElements(int arr[],int n)
 {
-    if(n==0 || n==1)
-        cout<<-1<<" "<<-1<<endl;  // edge case when only one element is present in array
-    sort(arr,arr+n);
-    int small=arr[1];
-    int large=arr[n-2];
-    cout<<"Second smallest is "<<small<<endl;
-    cout<<"Second largest is "<<large<<endl;
+    if(n==0 || n==1){
+        cout<<-1<<" "<<-1<<endl;      // edge case when only one element is present in array
+    }else{
+        sort(arr,arr+n);
+        int small=arr[1];
+        int large=arr[n-2];
+        cout<<"Second smallest is "<<small<<endl;
+        cout<<"Second largest is "<<large<<endl;
+    }     
+    
 }
 int main()
 {
-    int arr[]={1,2,4,6,7,5};
+    int arr[]={1,6,9,3,5};
     int n=sizeof(arr)/sizeof(arr[0]);
     getElements(arr,n);
     return 0;
